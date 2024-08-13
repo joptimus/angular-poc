eff16e76d43b1e5ea8aa2a68d391362bc96137cb
joptimus <jlewan27@gmail.com>
2024-08-13 09:07:12 -0500

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'angular.poc',
+  appName: 'angular-poc',
+  webDir: 'www'
+};
+
+export default config;
